eb653129cd4cd5df09adcc71d0012556407d3f28
AcroTaken <sven.nguyen@yahoo.com>
2026-03-09 08:28:02 -0500

    Template references

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,41 @@
 <script setup></script>
 
 <style lang="scss" scoped></style>
+<template>
+  <div class="card">
+    <h1 ref="title">Hello, Template Refs!</h1>
+    <input type="text" ref="input" />
+    <br />
+    <button @click="printDomElements">Print DOM elements in console log</button>
+    <button @click="changeTitle">Change title</button>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+
+let title = ref()
+const input = ref()
+
+function printDomElements() {
+  console.log(title.value)
+  console.log(input.value)
+}
+
+function changeTitle() {
+  title.value.innerText = 'Hello world!'
+}
+
+onMounted(() => {
+  input.value.focus() 
+})
+</script>
+
+<style scoped>
+.card {
+  background-color: purple;
+  color: white;
+  padding: 20px 10px;
+  margin-bottom: 10px;
+}
+</style>
